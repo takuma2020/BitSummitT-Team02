66680ef3030ea76f06e580f0998580cd6eec014b
takuma2020 <skskpnd20@gmail.com>
2023-05-25 16:05:58 +0900

    move

--- Assets/Script/CharactorMove.cs
+++ Assets/Script/CharactorMove.cs
@@ -8,54 +8,84 @@ public class CharactorMove : MonoBehaviour
     public GameObject target1;
     public GameObject target2;
     public GameObject target3;
-    public GameObject target4;
     public float speed;
-    public bool isTouch;
     public bool isRoute1;
     public bool isRoute2;
     public bool isRoute3;
+    public bool rayHit;
+    private Vector3 moveTarget;
 
     void Start()
     {
         speed = 0.1f;
-        target1 = GameObject.Find("MousePoint");
-        target2 = GameObject.Find("collider1");
-        target3 = GameObject.Find("collider2");
-        target4 = GameObject.Find("collider3");
-        isTouch = false;
+        target1 = GameObject.Find("collider1");
+        target2 = GameObject.Find("collider2");
+        target3 = GameObject.Find("collider3");
         isRoute1 = false;
         isRoute2 = false;
         isRoute3 = false;
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name == "MousePoint")
-        {
-            isTouch = true;
-        }
-
-        if (collision.gameObject.name == "collder1")
-        {
-            isRoute1 = true;
-        }
-
-        if (collision.gameObject.name == "collder2")
-        {
-            isRoute2 = true;
-        }
-
-        if (collision.gameObject.name == "collder3")
-        {
-            isRoute3 = true;
-        }
-    }
-
     void Update()
     {
-        if (isTouch == false)
+        if(Input.GetMouseButtonDown(0))
         {
-            transform.position = Vector3.MoveTowards(transform.position, target1.transform.position, speed);
+            RayCheck();
+        }
+
+        if(rayHit)
+        {
+            CharaMove();
+        }
+
+        
+    }
+
+    private void CharaMove()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveTarget, speed);
+    }
+
+    private void RayCheck()
+    {
+        Ray ray = new Ray();
+        RaycastHit hit = new RaycastHit();
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 mousePos = Input.mousePosition;
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity) && hit.collider == target1.GetComponent<Collider>())
+        {
+            rayHit = true;
+            if(isRoute1 == false)
+            {
+                moveTarget = target1.transform.position;
+                isRoute1 = true;
+            }
+        }
+
+        else if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity) && hit.collider == target2.GetComponent<Collider>())
+        {
+            rayHit = true;
+            if(isRoute1 == true && isRoute2 == false)
+            {
+                moveTarget = target2.transform.position;
+                isRoute2 = true;
+            }
+        }
+
+        else if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity) && hit.collider == target3.GetComponent<Collider>())
+        {
+            rayHit = true;
+            if (isRoute2 == true && isRoute3 == false)
+            {
+                moveTarget = target3.transform.position;
+                isRoute3 = true;
+            }
+        }
+
+        else
+        {
+            rayHit = false;
         }
     }
 }
