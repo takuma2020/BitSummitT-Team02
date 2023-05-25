0b4e62fbc2216277887270304713702288de01d1
takuma2020 <skskpnd20@gmail.com>
2023-05-25 15:11:53 +0900

    0525am

--- Assets/Script/CharactorMove.cs
+++ Assets/Script/CharactorMove.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class CharactorMove : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject target;
+    public GameObject target1;
+    public GameObject target2;
+    public GameObject target3;
+    public GameObject target4;
     public float speed;
     public bool isTouch;
     public bool isRoute1;
@@ -15,7 +18,10 @@ public class CharactorMove : MonoBehaviour
     void Start()
     {
         speed = 0.1f;
-        target = GameObject.Find("MousePoint");
+        target1 = GameObject.Find("MousePoint");
+        target2 = GameObject.Find("collider1");
+        target3 = GameObject.Find("collider2");
+        target4 = GameObject.Find("collider3");
         isTouch = false;
         isRoute1 = false;
         isRoute2 = false;
@@ -28,13 +34,28 @@ public class CharactorMove : MonoBehaviour
         {
             isTouch = true;
         }
+
+        if (collision.gameObject.name == "collder1")
+        {
+            isRoute1 = true;
+        }
+
+        if (collision.gameObject.name == "collder2")
+        {
+            isRoute2 = true;
+        }
+
+        if (collision.gameObject.name == "collder3")
+        {
+            isRoute3 = true;
+        }
     }
 
     void Update()
     {
         if (isTouch == false)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+            transform.position = Vector3.MoveTowards(transform.position, target1.transform.position, speed);
         }
     }
 }
